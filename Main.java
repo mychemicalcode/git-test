2903bcb551d1cff3787fc07453367d676889e757
mychemicalcode <jty7027@gmail.com>
2025-03-06 00:52:37 +0900

    git hub 최초 test

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String [] args) {
+
+        int result = 0 ;
+        int a = 0;
+        int b = 2;
+        
+
+        result = a + b;
+        System.out.println("result의 값은 " + result);
+        // 첫번째 test
+
+    }
+    
+}
